8dc1f04f0bdbfeaa1c1783a5e3d71f4638274dc7
FranRM15 <franjrm15@gmail.com>
2022-02-11 17:10:44 -0500

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+for (a = 97; a <= 122; a++)
+{
+	putchar(a);
+}
+printf("\n");
+for (a = 65; a <= 90; a++)
+	putchar(a);
+return (0);
+}
