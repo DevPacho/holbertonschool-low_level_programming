f49c8729415e322169f2227b12499bdf59d615b8
FranRM15 <franjrm15@gmail.com>
2022-02-10 22:07:00 -0500

    6. Size is not grandeur, and territory does not make a nation - Printing sizes in C with printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(longintType));
-	printf("Size of a long long int: %zu bytes(s)\n", sizeof(longlongintType));
-	printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
 	return (0);
 }
