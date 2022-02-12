4cccb11580f8ab6f5395020133af9c28d9ba213d
FranRM15 <franjrm15@gmail.com>
2022-02-11 19:08:16 -0500

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+for (a = 48; a <= 57; a++)
+{
+	putchar(a);
+}
+for (a = 97; a <= 102; a++)
+{
+	putchar(a);
+}
+printf("\n");
+return (0);
+}
