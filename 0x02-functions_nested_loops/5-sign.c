2dfa5a03ca140d972b94dd47b6f4b24445dde7f1
FranRM15 <franjrm15@gmail.com>
2022-02-15 17:26:58 -0500

    5. Sign (PACKAGED FILES)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of a number
+ * @n: parameter n
+ * Return: (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
