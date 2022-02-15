963f9ea155576b13d38b2b518588f19f57fd2e89
FranRM15 <franjrm15@gmail.com>
2022-02-15 12:42:51 -0500

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game PACKAGED FILES

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
