78ec87badf9ae481ad35671dc4c29da5cab7bf1f
FranRM15 <franjrm15@gmail.com>
2022-02-16 00:29:59 -0500

    11. 98 Battery Street, the OG (PACKAGED FILES)

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: parameter n
@@ -7,5 +8,15 @@
 
 void print_to_98(int n)
 {
-
+	if (n <= 97)
+	{
+		for (; n <= 97; n++)
+			printf("%d, ", n);
+	}
+	else if (n > 98)
+	{
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	}
+	printf("%d\n", n);
 }
