f718cd68699c26e91adc3db5c985006dfe673c59
FranRM15 <franjrm15@gmail.com>
2022-02-16 16:18:57 -0500

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function that prints positive or negative int
+ * @i: parameter i
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	printf("%d is zero\n", i);
+}
