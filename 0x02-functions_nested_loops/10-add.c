9f0e75ec0558d2f462949c4cab5e8685bda4b825
FranRM15 <franjrm15@gmail.com>
2022-02-15 23:49:27 -0500

    10. a + b (PACKAGES FILES)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ * @a: int #1 to add
+ * @b: int #2 to add
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
