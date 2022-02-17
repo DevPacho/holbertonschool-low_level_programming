d1ce1cbaa2e1d9893592587de5d73ed417f1c354
FranRM15 <franjrm15@gmail.com>
2022-02-17 16:12:06 -0500

    fixed 2. 0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
-i#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -6,4 +6,4 @@ i#ifndef MAIN_H
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
-#endif /* MAIN_H */
+#endif /* MAIN_H*/
