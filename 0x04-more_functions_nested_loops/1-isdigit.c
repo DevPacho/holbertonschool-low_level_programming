1e1dbf199c3fa7fff8c3d7317531725578e47dba
FranRM15 <franjrm15@gmail.com>
2022-02-18 15:46:23 -0500

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,11 @@
  * Return: Always 0 (Success)
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-        if (c >= '0' && c <= '9')
-        {
-                return (1);
-        }
-        return (0);
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
 }
