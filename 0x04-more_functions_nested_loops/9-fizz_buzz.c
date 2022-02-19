bc8fa278174185be40f250977c5bb811cd633d50
FranRM15 <franjrm15@gmail.com>
2022-02-18 21:23:50 -0500

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			if (a % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (a % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
+		{
+			printf("%d", a);
+		}
+		if (a != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
