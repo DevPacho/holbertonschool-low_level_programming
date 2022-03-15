887527cf9f3ef4d411776de8c78421676c3e248b
FranRM15 <franjrm15@gmail.com>
2022-03-14 21:09:43 -0500

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGPOPPY
+#define DOGPOPPY
+
+/**
+ * struct dog - struct dog Poppy.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
