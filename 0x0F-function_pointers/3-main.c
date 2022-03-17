605b9c5324397fc66d4b7c4fbd7f2c45832d24ca
FranRM15 <franjrm15@gmail.com>
2022-03-17 00:21:03 -0500

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * main - main function.
+ * @argc: number of words (arguments)
+ * entered in the line where the program is called.
+ * @argv: pointers to each string formed
+ * by entering the words (arguments).
+ * Return: 0 (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*ptr)(int, int);
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	ptr = get_op_func(argv[2]);
+
+	if (ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", ptr(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b.
+ * @a: integer to add.
+ * @b: integer to add.
+ * Return: add.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b.
+ * @a: integer to subtract.
+ * @b: integer to subtract.
+ * Return: subtract.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b.
+ * @a: integer to multiply.
+ * @b: integer to multiply.
+ * Return: multiplication.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of a by b.
+ * @a: integer to divide.
+ * @b: integer to divide.
+ * Return: division.
+ */
+
+int op_div(int a, int b)
+{
+	if (a == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b.
+ * @a: integer to take the rest.
+ * @b: integer to take the rest.
+ * Return: remainder of the division.
+ */
+
+int op_mod(int a, int b)
+{
+	if (a == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
