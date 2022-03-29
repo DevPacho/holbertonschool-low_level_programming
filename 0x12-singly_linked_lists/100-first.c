10739c9753231c206de1d1fe9e81d67e19f23150
FranRM15 <franjrm15@gmail.com>
2022-03-29 16:51:16 -0500

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * beforethemain - Write a function that prints a message
+ * before the main.
+ */
+
+void beforethemain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
