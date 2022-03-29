ad586e7a7d92fe042ba2f0ba5b9e6066bc48e58c
FranRM15 <franjrm15@gmail.com>
2022-03-29 10:18:07 -0500

    [ADDED] Free memory

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,10 @@ list_t *add_node(list_t **head, const char *str)
 	newnode = malloc(sizeof(list_t));
 
 	if (newnode == NULL)
+	{
+		free(newnode);
 		return (NULL);
+	}
 
 	for (; str[a]; a++)
 		;
