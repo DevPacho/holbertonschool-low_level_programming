eddc3c297aa1032aec3c4f9c2051b7dbf7ec0b16
FranRM15 <franjrm15@gmail.com>
2022-04-01 16:53:26 -0500

    [FIX] betty style

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * pop_listint - function that deletes the head node of a listint_t linked list.
+ * pop_listint - function that deletes the head node of a
+ * listint_t linked list.
  * @head: head of the list.
  * Return: the head node’s data (n) or return 0 if the linked list is empty.
  */
