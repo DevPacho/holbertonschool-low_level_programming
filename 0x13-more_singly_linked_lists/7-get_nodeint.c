b882d133e1b4d261e007945f96011780654a520d
FranRM15 <franjrm15@gmail.com>
2022-04-01 20:02:26 -0500

    [ADDED] validation for NULL case of head in loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 
-	for (; a < index && head != NULL; a++)
+	for (; a < index && head; a++)
 		tonth = tonth->next;
 
 	return (tonth);
