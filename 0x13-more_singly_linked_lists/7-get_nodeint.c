67c818a1f0d08efc165596544f478df8d4dc135a
FranRM15 <franjrm15@gmail.com>
2022-04-01 19:12:22 -0500

    [FIXED] betty style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - function that returns the nth node of a listint_t linked list.
+ * get_nodeint_at_index - function that returns the nth node
+ * of a listint_t linked list.
  * @head: head of the list.
  * @index: index of the node.
  * Return: the nth node of a listint_t linked list
- * or NULL if the node does not exist. 
+ * or NULL if the node does not exist.
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -14,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int a = 0;
 
 	tonth = head;
-	if (!head && !tonth)
+	if (!head)
 		return (NULL);
 
 	for (; a < index; a++)
