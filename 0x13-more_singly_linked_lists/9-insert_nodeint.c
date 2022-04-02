0c97526e337933bb4c7537cd25cdb33814921238
FranRM15 <franjrm15@gmail.com>
2022-04-02 17:50:26 -0500

    [FIXED] betty style again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 	}
 
-	newnode2 = malloc(sizeof(listint_t))
+	newnode2 = malloc(sizeof(listint_t));
 
 	if (newnode2 == NULL)
 		return (NULL);
