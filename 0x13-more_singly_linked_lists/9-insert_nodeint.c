8fb15749d9c67bd8a030c083bfb14de941a02af9
FranRM15 <franjrm15@gmail.com>
2022-04-02 17:38:07 -0500

    [FIXED] output by NULL verification of newnode

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (; a < idx - 1 && newnode; a++)
+		for (; a < idx - 1 && newnode != NULL; a++)
 			newnode = newnode->next;
 	}
 
