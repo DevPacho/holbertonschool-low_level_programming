45745b4b98bc66288b8b08574275f20579a16cc0
FranRM15 <franjrm15@gmail.com>
2022-04-01 19:49:17 -0500

    Trying without aux pointer

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,15 +11,13 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tonth;
 	unsigned int a = 0;
 
-	tonth = head;
+	if (!head)
+		return (NULL);
+
 	for (; a < index; a++)
-	{
-		tonth = tonth->next;
-		if (!head)
-			return (NULL);
-	}
-	return (tonth);
+		head = head->next;
+
+	return (head);
 }
