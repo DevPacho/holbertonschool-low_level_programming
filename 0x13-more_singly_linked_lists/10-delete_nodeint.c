0d6233867853debd9e6483508bfddbaf25dfcd06
FranRM15 <franjrm15@gmail.com>
2022-04-01 23:40:34 -0500

    [FIXED] betty style

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - function that deletes the node 
+ * delete_nodeint_at_index - function that deletes the node
  * at index index of a listint_t linked list.
- * @head: head of the list. 
+ * @head: head of the list.
  * @index: index of the node that should be deleted.
  * Return: 1 if it succeeded, -1 if it failed.
  */
@@ -12,6 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int a = 0;
 	listint_t *newnode = *head, *newnode2;
+
 	if (*head == NULL || head == NULL)
 		return (-1);
 
