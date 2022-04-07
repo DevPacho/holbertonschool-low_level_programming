ad3202ba2848bf94d90f98c60561e5db1aba3420
FranRM15 <franjrm15@gmail.com>
2022-04-06 22:37:45 -0500

    [FIXED] output by write parameter

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	for (; text_content[a]; a++)
 		;
 
-	towrite = write(STDOUT_FILENO, text_content, a);
+	towrite = write(filedescrp, text_content, a);
 
 	if (towrite == -1)
 		return(-1);
