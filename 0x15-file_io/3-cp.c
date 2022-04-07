fcb57d4268966116a70ff70a7609bf135389bd03
FranRM15 <franjrm15@gmail.com>
2022-04-07 11:44:01 -0500

    [FIXED] betty style more than 40 lines

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,10 +14,8 @@ int main(int argc, char *argv[])
 	ssize_t toread, towrite;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
 	filedescrp = open(argv[1], O_RDONLY);
 	if (filedescrp == -1)
 	{
@@ -26,34 +24,24 @@ int main(int argc, char *argv[])
 	}
 	filedescrp2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
 	if (filedescrp2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
 	while (toread > 0)
 	{
 		toread = read(filedescrp, buffer, 1024);
 		if (toread == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1])
 			exit(98);
 		}
 		towrite = write(filedescrp2, buffer, toread);
 		if (towrite == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(filedescrp) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filedescrp);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filedescrp), exit(100);
+
 	if (close(filedescrp2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filedescrp2);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filedescrp2), exit(100);
 	return (0);
 }
