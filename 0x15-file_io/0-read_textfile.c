13b7f2acbd3cd05a036f002f86251e71a4740115
FranRM15 <franjrm15@gmail.com>
2022-04-06 20:17:17 -0500

    [FIXED] betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,15 +11,14 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int filedescrp; /** file descriptor (int value to return) **/
-	ssize_t number_letters; /** maximum number of letters to read **/ 
+	ssize_t number_letters; /** maximum number of letters to read **/
 	ssize_t towrite; /** for the return value **/
 	char *buffer; /** saves the read characters **/
 
 	if (filename == NULL)
 		return (0);
 
-	filedescrp = open(filename, O_RDONLY); /** opens file
-										  with read only mode **/
+	filedescrp = open(filename, O_RDONLY); /** opens file with read only mode **/
 	if (filedescrp == -1) /** if fails **/
 	{
 		close(filedescrp);
@@ -39,8 +38,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (number_letters == -1)
 		return (0);
 
-	towrite = write(STDOUT_FILENO, buffer, number_letters); /** Writes everything 
-													 to the standard output **/
+	towrite = write(STDOUT_FILENO, buffer, number_letters);
+	/** Writes everything to the standard output **/
+
 	if (towrite == -1)
 		return (0);
 
