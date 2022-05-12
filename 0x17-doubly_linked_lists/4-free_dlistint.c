0b2f48d3d614475350522129d7976590fd2ac836
DevPacho <frankjose2004@gmail.com>
2022-05-12 02:19:03 -0500

    [FIXED] betty style, not spaces, are tabs

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
- * free_dlistint - function that frees a doubly linked list.
- * @head: head of the list.
- */
+* free_dlistint - function that frees a doubly linked list.
+* @head: head of the list.
+*/
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *to_free;
+	dlistint_t *to_free;
 
-    while (head)
-    {
-        to_free = head;
-        head = head->next;
-        free(to_free);
-    }
+	while (head)
+	{
+		to_free = head;
+		head = head->next;
+		free(to_free);
+	}
 }
