f59c34be2c48b344db50508e276636179c732390
DevPacho <frankjose2004@gmail.com>
2022-06-06 15:07:14 -0500

    [FIXED] bettystyle by indent with tabs and not with spaces

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,13 +4,14 @@
 * key_index - function that gives you the index of a key.
 * @key: the key.
 * @size: size of the array of the hash table.
-* Return: the index at which the key/value pair should be stored in the array of the hash table.
+* Return: the index at which the key/value pair should be
+* stored in the array of the hash table.
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    int keyvalue_index;
+	int keyvalue_index;
 
-    keyvalue_index = hash_djb2(key) % size;
-    return (keyvalue_index);
+	keyvalue_index = hash_djb2(key) % size;
+	return (keyvalue_index);
 }
