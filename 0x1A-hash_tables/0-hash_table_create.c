828c8f02c8a542c434e2002598f370cf843e5ba0
DevPacho <frankjose2004@gmail.com>
2022-06-06 12:31:05 -0500

    [FIXED] bettystyle

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,22 +9,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int creating;
+	hash_table_t *table;
+	unsigned long int creating;
 
-    table = malloc(sizeof(hash_table_t));
-    if (!table)
-        return NULL;
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 
-    table->array = malloc(sizeof(hash_node_t*) * size);
-    if (!table->array)
-    {
-        free(table->array);
-        return NULL;
-    }
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+	{
+		free(table->array);
+		return (NULL);
+	}
 
-    table->size = size;
-    for (; creating < size; creating++)
-        table->array[creating] = NULL;
-    return table;
+	table->size = size;
+	for (; creating < size; creating++)
+		table->array[creating] = NULL;
+	return (table);
 }
