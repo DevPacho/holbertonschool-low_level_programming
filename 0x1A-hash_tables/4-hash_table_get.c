cab1c5a4592f9fec89c4769f92ddaa74c2fbe62f
DevPacho <frankjose2004@gmail.com>
2022-06-06 21:20:26 -0500

    [FIXED] indentation with tabs

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,21 +10,21 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int table_index;
-    hash_node_t *node_to_check = NULL;
+	unsigned long int table_index;
+	hash_node_t *node_to_check = NULL;
 
-    if (!key)
-        return (NULL);
+	if (!key)
+		return (NULL);
 
-    table_index = key_index((unsigned char *)key, ht->size);
-    node_to_check = ht->array[table_index];
+	table_index = key_index((unsigned char *)key, ht->size);
+	node_to_check = ht->array[table_index];
 
-    while (node_to_check)
-    {
-        if (strcmp(node_to_check->key, key) == 0)
-            return(node_to_check->value);
-        node_to_check = node_to_check->next;
-    }
+	while (node_to_check)
+	{
+		if (strcmp(node_to_check->key, key) == 0)
+			return (node_to_check->value);
+		node_to_check = node_to_check->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
