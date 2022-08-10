4eee5eac40f92198739f2b4986453fc04267e6d8
DevPacho <frankjose2004@gmail.com>
2022-08-10 14:12:28 -0500

    [UPDATED] function description

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
-* binary_search - function that searches for a value in a sorted array of integers
-* using the 'Binary search algorithm'.
+* binary_search - function that searches for a value in a
+* sorted array of integers using the 'Binary search algorithm'.
 *
 * @array: is a pointer to the first element of the array to search in.
 * @size: is the number of elements in the array.
